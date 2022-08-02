1f2337bfe7480f3e1a9fc5530cc86859b5556f8b
Sam <samfagelman@hotmail.com>
2022-08-02 11:29:59 +0100

    add endpoint GET /api/articles/:article_id

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require("express");
-const { getTopics } = require("./controllers/topics.controllers");
+const {
+  getTopics,
+  getArticleById,
+} = require("./controllers/topics.controllers");
 
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Invalid route" });
 });
 
 //////////////////////////////////
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(404).send({ msg: "Invalid Request" });
+  }
+});
 
 module.exports = app;
--- models/topics.models.js
+++ models/topics.models.js
@@ -3,3 +3,18 @@ const db = require("../db/connection.js");
 exports.selectTopics = () => {
   return db.query("SELECT * FROM topics;").then((topics) => topics.rows);
 };
+
+exports.selectArticleById = (articleId) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [articleId])
+    .then((article) => {
+      if (article.rows.length === 0) {
+        return Promise.reject({
+          status: 400,
+          msg: "Article does not exist",
+        });
+      } else {
+        return article.rows[0];
+      }
+    });
+};
