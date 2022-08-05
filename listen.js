b851d2eb8c02a53491f7ea5802b8d092893db451
Sam <samfagelman@hotmail.com>
2022-08-05 14:59:21 +0100

    fixed errors in heroku setup

--- listen.js
+++ listen.js
@@ -1,6 +1,8 @@
-const express = require("express");
+// const express = require("express");
 
-const app = express();
+// const app = express();
+
+const app = require("./app");
 
 const { PORT = 9090 } = process.env;
 
